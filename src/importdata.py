ac56c52b7685b40ea285da1d37a61f3d59d1c7b2
Berchie <agyemangnti@gmail.com>
2024-02-29 19:57:16 +0100

    added new feature

--- src/importdata.py
+++ src/importdata.py
@@ -1,19 +1,20 @@
-import datetime
 import os
 import re
 import sys
 from pathlib import Path
-import time
-import pandas
+from time import localtime, strftime
 from src.functions import check_internet_connection, write_result_csv
 from dotenv import dotenv_values
 import requests
 import json
 import logging.config
 import yaml
+import csv
+from src.sendemail import email_notification
+
 
 # add the path of the new different folder (the folder from where we want to import the modules)
-# sys.path.insert(0, './src')
+sys.path.insert(0, './src')
 
 # import the customise logger YAML dictionary configuration file
 # logging any error or any exception to a log file
@@ -24,49 +25,84 @@ with open(f'{os.path.abspath(".")}/config_log.yaml', 'r') as f:
 logger = logging.getLogger(__name__)
 
 
-def to_csv_file():
-    DATA_DIR = f'{os.path.abspath(".")}/data'
-    MBC_TVISIT_CSV_DIR = f'{os.path.abspath(".")}/data/csv/mbc_tvisit_labresult.csv'
-    MBC_FVISIT_CSV_DIR = f'{os.path.abspath(".")}/data/csv/mbc_fevervisit_labresult.csv'
-    PEDVAC_CSV_DIR = f'{os.path.abspath(".")}/data/csv/pedvac_labresult.csv'
-    CHECK_DATE = datetime.date.today()
-    CHECK_FILE_DATE_STRING = datetime.date.today().strftime('%Y%m%d')
+# writing the imported results to CSV file
+def result_csv():
+    csv_file = f'{os.path.abspath(".")}/data/csv/haematology.csv'
 
-    # list items in the data dir
-    for item in Path(DATA_DIR).iterdir():
-        # print(item)
-        if item.is_file() and item.name.split('.')[-1] == 'csv':
-            if CHECK_DATE == datetime.date.fromtimestamp(item.lstat().st_atime):
-                # print(f'file: {item.name}\n\tdate:{datetime.datetime.fromtimestamp(item.lstat().st_atime)}')  # strftime("%Y%m%d")
-                if item.name.split('_')[0] == CHECK_FILE_DATE_STRING and item.name.split('_')[1] == 'mbc':
-                    # print(item.name.split('_'))
-                    # print(item)
-                    if 'mbc_t612visit_labresult.csv' in item.name:
-                        df_tv = pandas.read_csv(item)
-                        if os.path.exists(MBC_TVISIT_CSV_DIR) and os.path.getsize(MBC_TVISIT_CSV_DIR) > 0:
-                            df_tv.to_csv(MBC_TVISIT_CSV_DIR, index=False, header=False, mode='a')
-                        else:
-                            df_tv.to_csv(MBC_TVISIT_CSV_DIR, index=False)
-                    else:
-                        # mbc_fevervisit_labresult.csv
-                        df_fv = pandas.read_csv(item)
-                        if os.path.exists(MBC_FVISIT_CSV_DIR) and os.path.getsize(MBC_TVISIT_CSV_DIR) > 0:
-                            df_fv.to_csv(MBC_FVISIT_CSV_DIR, index=False, header=False, mode='a')
-                        else:
-                            df_fv.to_csv(MBC_FVISIT_CSV_DIR, index=False)
-                else:
-                    # print(item.name.split('_'))
-                    # pedvac_labresult.csv
-                    pedvac_df = pandas.read_csv(item)
-                    if os.path.exists(PEDVAC_CSV_DIR) and os.path.getsize(PEDVAC_CSV_DIR) > 0:
-                        pedvac_df.to_csv(PEDVAC_CSV_DIR, index=False, header=False, mode='a')
-                    else:
-                        pedvac_df.to_csv(PEDVAC_CSV_DIR, index=False)
+    # open or read the json file
+    with open(f'{os.path.abspath(".")}/data/import_data.json') as json_file:
+        json_results = json.load(json_file)
+
+    # open the csv file
+    if os.path.isfile(csv_file) and os.path.getsize(csv_file) > 0:
+        data_file = open(csv_file, 'a', newline='')
+    else:
+        data_file = open(csv_file, 'w', newline='')
+
+    # create the csv writer object
+    csv_writer = csv.writer(data_file)
+
+    # Counter variable used for writing
+    # headers to the CSV file
+    # if the CSV file exist and not empty set the counter to 1 to write the rows
+    if os.path.isfile(csv_file) and os.path.getsize(csv_file) > 0:
+        counter = 1
+    else:
+        counter = 0
+
+    for result in json_results:
+
+        if counter == 0:
+            # Writing headers of CSV file
+            header = result.keys()
+            csv_writer.writerow(header)
+            counter += 1
+
+        # writing data of the CSV file
+        csv_writer.writerow(result.values())
+
+
+# convert json data to CSV file for the daily run
+def json_csv():
+    csv_file = f'{os.path.abspath(".")}/data/daily_result/{strftime("%Y%m%d", localtime())}_haematology.csv'
+
+    # open or read the json file
+    with open(f'{os.path.abspath(".")}/data/import_data.json') as json_file:
+        json_results = json.load(json_file)
+
+    # open the csv file
+    if os.path.isfile(csv_file) and os.path.getsize(csv_file) > 0:
+        data_file = open(csv_file, 'a', newline='')
+    else:
+        data_file = open(csv_file, 'w', newline='')
+
+    # create the csv writer object
+    csv_writer = csv.writer(data_file)
+
+    # Counter variable used for writing
+    # headers to the CSV file
+    # if the CSV file exist and not empty set the counter to 1 to write the rows
+    if os.path.isfile(csv_file) and os.path.getsize(csv_file) > 0:
+        counter = 1
+    else:
+        counter = 0
+
+    for result in json_results:
+
+        if counter == 0:
+            # Writing headers of CSV file
+            header = result.keys()
+            csv_writer.writerow(header)
+            counter += 1
+
+        # writing data of the CSV file
+        csv_writer.writerow(result.values())
 
 
 def data_import(project_id):
-    m19_csv_file = 'import_m19_data.csv'
-    p21_csv_file = 'import_p21_data.csv'
+
+    # m19_csv_file = 'import_m19_data.csv'
+    # p21_csv_file = 'import_p21_data.csv'
     # record = f'{os.path.abspath("..")}/data/import_data.json'
     record = './data/import_data.json'
 
@@ -75,109 +111,58 @@ def data_import(project_id):
         env_config = dotenv_values(f"{os.path.abspath('.')}/.env")
 
         # read the file import_json file
-        # if os.path.getsize(record) != 0:
-        #     with open(record) as jf:
-        #         import_record = json.load(jf)
-        #
-        # data = json.dumps(import_record)
-
         if os.path.getsize(record) != 0:
+            with open(record) as jf:
+                import_record = json.load(jf)
 
-            # wrap the importing processing with click progressbar
-            # with click.progressbar(label='Processing data to import into REDCap database', length=100, fill_char=u'█', empty_char='', width=100) as data_bar:
+            data = json.dumps(import_record)
 
-            # convert json data to csv object
-            # use the read the csv object or file to be imported into REDCap database
-            df_obj = pandas.read_json(record, orient='records')
-            if project_id == 'M19':
-                df_obj.to_csv(f'{os.path.abspath(".")}/data/{m19_csv_file}', index=False)
-
-                with open(f'{os.path.abspath(".")}/data/{m19_csv_file}') as csv_file:
-                    data = csv_file.read()
-
-            else:
-                df_obj.to_csv(f'{os.path.abspath(".")}/data/{p21_csv_file}', index=False)
-
-                with open(f'{os.path.abspath(".")}/data/{p21_csv_file}') as csv_file:
-                    data = csv_file.read()
-
-            # data_bar.update(5)
-            # time.sleep(1.01)
-            # data_bar.update(20)
-            # time.sleep(1.25)
-            # data_bar.update(25)
-            # time.sleep(2)
-            # data_bar.update(25)
-            # time.sleep(3)
-            # data_bar.update(25)
-
-            # with open('./data/import_data.csv') as csv_file:
-            #     data = csv_file.read()
-
-            # click.echo(data)
-
-            if project_id == 'M19':
-                api_token = env_config['M19_API_TOKEN']
-            else:
-                api_token = env_config['P21_API_TOKEN']
+            # API TOKEN for the REDCap database
+            api_token = env_config['LAB_API_TOKEN']
 
             fields = {
                 'token': api_token,
                 'content': 'record',
                 'action': 'import',
-                'format': 'csv',  # json csv
+                'format': 'json',  # json csv
                 'type': 'flat',
                 'overwriteBehavior': 'normal',  # overwrite normal
                 'data': data,
-                'returnContent': 'count',  # count #ids
+                'returnContent': 'ids',  # count #ids
                 'returnFormat': 'json'
             }
 
             # with click.progressbar(label='Importing data into the REDCap database', length=100, fill_char=u'█', empty_char='', width=100) as import_data_bar:
             # check for internet is available or REDCap server is online(available)
-            if check_internet_connection("https://redcap-testing.bibbox.bnitm.de/"):
+            if check_internet_connection("https://redcap.bibbox.bnitm.de/"):
                 # import records into the REDCap database
-                r = requests.post(env_config['API_URL'], data=fields)
+                r = requests.post(env_config['LAB_API_URL'], data=fields)
                 # print(f'HTTP Status: {str(r.status_code)}')
-                count = r.json()
+                res = r.json()
+
+                count = len(res)
 
                 if r.status_code == 200:
-                    logging.info(f"{count.get('count', 0)} of {project_id} record(s) were imported successfully!!!")
-                    # print(count)
+                    logging.info(f"{count} of {project_id} record(s) were imported successfully!!!")
+
+                    import_success_msg = f"{count} of {project_id} record(s) were imported successfully!"
+                    sample_ids = res
+
+                    # email notification
+                    email_notification(import_success_msg, sample_ids)
+
+                    # convert json to CSV
+                    json_csv()
+
+                    # write the import data to CSV file
+                    result_csv()
+
                 else:
-                    count = re.sub(r"[\\{}]", "", r.text)
-                    logger.error(f'HTTP Status:{r.status_code} - {count}')
-
-                # delete the import_data.csv file
-                if project_id == 'M19':
-                    if os.path.exists(f'./data/{m19_csv_file}'):  # check if the file exist before deleting it
-                        os.remove(f'./data/{m19_csv_file}')
-                else:
-                    if os.path.exists(f'./data/{p21_csv_file}'):
-                        os.remove(f'./data/{p21_csv_file}')
-
-                # write the data to csv file(s)
-                write_result_csv(record, project_id)
-
-                # copy the transfer or imported data to a csv file in the cvs dir
-                to_csv_file()
-
-            else:
-                # write the data to csv file(s) if there is no internet connection
-                write_result_csv(record, project_id)
-
-            # import_data_bar.update(5)
-            # time.sleep(1.01)
-            # import_data_bar.update(20)
-            # time.sleep(1.25)
-            # import_data_bar.update(25)
-            # time.sleep(2)
-            # import_data_bar.update(25)
-            # time.sleep(3)
-            # import_data_bar.update(25)
+                    error_msg = re.sub(r"[\\{}]", "", r.text)
+                    print(f'HTTP Status:{r.status_code} - {error_msg}')
 
         else:
-            logger.info(f'No {project_id} data to import.')
+            print(f'No {project_id} data to import.')
             # click.echo(f'No {project_id} data to import.')
 
         # when record successful imported write it to csv(import_data_[date&time])
