0c93235b3afd0cb39b114544b966f2e32db4f83b
Berchie <agyemangnti@gmail.com>
2024-06-28 15:00:58 +0200

    add transfer of result for RM cases

--- redcapconnector/importdata_smart.py
+++ redcapconnector/importdata_smart.py
@@ -1,17 +1,17 @@
 import os
 import re
-import sys
-from pathlib import Path
+# import sys
+# from pathlib import Path
 from time import localtime, strftime
 from redcapconnector.functions import check_internet_connection, write_result_csv
 from dotenv import dotenv_values, load_dotenv
 import requests
 import json
 import logging.config
-import yaml
+# import yaml
 import logging
 import csv
-from redcapconnector.sendemail import email_notification
+# from redcapconnector.sendemail import email_notification
 from loguru import logger
 from redcapconnector.config.log_config import handlers
 
@@ -33,8 +33,10 @@ else:
 def result_csv_smart(sample_type, jsonfile, case_type):
     if case_type == "UM":
         case = 'um'
-    else:
+    elif case_type == "SM":
         case = 'sm'
+    else:
+        case = 'rm'
 
     if sample_type == 'EDTA Blood':
         csv_file = os.path.join(os.path.dirname(__file__), "data", "csv", f"smart_{case}_haematology.csv")
@@ -45,33 +47,53 @@ def result_csv_smart(sample_type, jsonfile, case_type):
     with open(os.path.join(os.path.dirname(__file__), "data", jsonfile)) as json_file:
         json_results = json.load(json_file)
 
-    # open the csv file
-    if os.path.isfile(csv_file) and os.path.getsize(csv_file) > 0:
-        data_file = open(csv_file, 'a', newline='')
-    else:
-        data_file = open(csv_file, 'w', newline='')
+        # open the csv file
+        if os.path.isfile(csv_file) and os.path.getsize(csv_file) > 0:
+            with open(csv_file, 'a', newline='', encoding='utf-8-sig') as data_file:
+                csv_writer = csv.DictWriter(data_file, json_results[0].keys())
+                # csv_writer.writeheader()
 
-    # create the csv writer object
-    csv_writer = csv.writer(data_file)
+                for row in json_results:
+                    new_row = {
+                        "id_um": row["id_um"],
+                        "redcap_event_name": row["redcap_event_name"],
+                        "redcap_data_access_group": row["redcap_data_access_group"],
+                        "visit_no_edta": row["visit_no_edta"],
+                        "hgb_edta": row["hgb_edta"],
+                        "mon_edta": row["mon_edta"],
+                        "bas_edta": row["bas_edta"],
+                        "rbc_edta": row["rbc_edta"],
+                        "lym_edta": row["lym_edta"],
+                        "hct_edta": row["hct_edta"],
+                        "eosi_edta": row["eosi_edta"],
+                        "wbc_edta": row["wbc_edta"],
+                        "neu_edta": row["neu_edta"],
+                        "plt_edta": row["plt_edta"]
+                    }
+                    csv_writer.writerow(new_row)
+        else:
+            with open(csv_file, 'a', newline='', encoding='utf-8-sig') as data_file:
+                csv_writer = csv.DictWriter(data_file, json_results[0].keys())
+                csv_writer.writeheader()
 
-    # Counter variable used for writing
-    # headers to the CSV file
-    # if the CSV file exist and not empty set the counter to 1 to write the rows
-    if os.path.isfile(csv_file) and os.path.getsize(csv_file) > 0:
-        counter = 1
-    else:
-        counter = 0
-
-    for result in json_results:
-
-        if counter == 0:
-            # Writing headers of CSV file
-            header = result.keys()
-            csv_writer.writerow(header)
-            counter += 1
-
-        # writing data of the CSV file
-        csv_writer.writerow(result.values())
+                for row in json_results:
+                    new_row = {
+                        "id_um": row["id_um"],
+                        "redcap_event_name": row["redcap_event_name"],
+                        "redcap_data_access_group": row["redcap_data_access_group"],
+                        "visit_no_edta": row["visit_no_edta"],
+                        "hgb_edta": row["hgb_edta"],
+                        "mon_edta": row["mon_edta"],
+                        "bas_edta": row["bas_edta"],
+                        "rbc_edta": row["rbc_edta"],
+                        "lym_edta": row["lym_edta"],
+                        "hct_edta": row["hct_edta"],
+                        "eosi_edta": row["eosi_edta"],
+                        "wbc_edta": row["wbc_edta"],
+                        "neu_edta": row["neu_edta"],
+                        "plt_edta": row["plt_edta"]
+                    }
+                    csv_writer.writerow(new_row)
 
 
 # convert json data to CSV file for the daily run (SMART Project)
@@ -79,8 +101,10 @@ def result_csv_smart(sample_type, jsonfile, case_type):
 def json_csv_smart(smart_type, jsonfile, case_type):
     if case_type == "UM":
         case = 'um'
-    else:
+    elif case_type == "SM":
         case = 'sm'
+    else:
+        case = 'rm'
 
     if smart_type == 'EDTA Blood':
         csv_file = os.path.join(os.path.dirname(__file__), "data", "daily_result", f"{strftime("%Y%m%d", localtime())}_smart_{case}_haematology.csv")
@@ -91,33 +115,53 @@ def json_csv_smart(smart_type, jsonfile, case_type):
     with open(os.path.join(os.path.dirname(__file__), "data", jsonfile)) as json_file:
         json_results = json.load(json_file)
 
-    # open the csv file
-    if os.path.isfile(csv_file) and os.path.getsize(csv_file) > 0:
-        data_file = open(csv_file, 'a', newline='')
-    else:
-        data_file = open(csv_file, 'w', newline='')
+        # open the csv file
+        if os.path.isfile(csv_file) and os.path.getsize(csv_file) > 0:
+            with open(csv_file, 'a', newline='', encoding='utf-8-sig') as data_file:
+                csv_writer = csv.DictWriter(data_file, json_results[0].keys())
+                # csv_writer.writeheader()
 
-    # create the csv writer object
-    csv_writer = csv.writer(data_file)
+                for row in json_results:
+                    new_row = {
+                        "id_um": row["id_um"],
+                        "redcap_event_name": row["redcap_event_name"],
+                        "redcap_data_access_group": row["redcap_data_access_group"],
+                        "visit_no_edta": row["visit_no_edta"],
+                        "hgb_edta": row["hgb_edta"],
+                        "mon_edta": row["mon_edta"],
+                        "bas_edta": row["bas_edta"],
+                        "rbc_edta": row["rbc_edta"],
+                        "lym_edta": row["lym_edta"],
+                        "hct_edta": row["hct_edta"],
+                        "eosi_edta": row["eosi_edta"],
+                        "wbc_edta": row["wbc_edta"],
+                        "neu_edta": row["neu_edta"],
+                        "plt_edta": row["plt_edta"]
+                    }
+                    csv_writer.writerow(new_row)
+        else:
+            with open(csv_file, 'a', newline='', encoding='utf-8-sig') as data_file:
+                csv_writer = csv.DictWriter(data_file, json_results[0].keys())
+                csv_writer.writeheader()
 
-    # Counter variable used for writing
-    # headers to the CSV file
-    # if the CSV file exist and not empty set the counter to 1 to write the rows
-    if os.path.isfile(csv_file) and os.path.getsize(csv_file) > 0:
-        counter = 1
-    else:
-        counter = 0
-
-    for result in json_results:
-
-        if counter == 0:
-            # Writing headers of CSV file
-            header = result.keys()
-            csv_writer.writerow(header)
-            counter += 1
-
-        # writing data of the CSV file
-        csv_writer.writerow(result.values())
+                for row in json_results:
+                    new_row = {
+                        "id_um": row["id_um"],
+                        "redcap_event_name": row["redcap_event_name"],
+                        "redcap_data_access_group": row["redcap_data_access_group"],
+                        "visit_no_edta": row["visit_no_edta"],
+                        "hgb_edta": row["hgb_edta"],
+                        "mon_edta": row["mon_edta"],
+                        "bas_edta": row["bas_edta"],
+                        "rbc_edta": row["rbc_edta"],
+                        "lym_edta": row["lym_edta"],
+                        "hct_edta": row["hct_edta"],
+                        "eosi_edta": row["eosi_edta"],
+                        "wbc_edta": row["wbc_edta"],
+                        "neu_edta": row["neu_edta"],
+                        "plt_edta": row["plt_edta"]
+                    }
+                    csv_writer.writerow(new_row)
 
 
 def data_import(sample):
@@ -127,7 +171,7 @@ def data_import(sample):
 
     # create for loop import the two json files
     try:
-        case_type = ("UM", "SM")
+        case_type = ("UM", "RM", "SM")
 
         for case in case_type:
 
@@ -136,6 +180,14 @@ def data_import(sample):
 
                 # API TOKEN for the REDCap database
                 api_token = os.environ['SMART_UM_API_TOKEN']
+
+                json_file = "smart_um_import_data.json"
+            elif case == "RM":
+
+                record = os.path.join(os.path.dirname(__file__), "data", "smart_rm_import_data.json")
+
+                # API TOKEN for the REDCap database
+                api_token = os.environ['SMART_SM_API_TOKEN']
 
                 json_file = "smart_um_import_data.json"
 
@@ -206,9 +258,9 @@ def data_import(sample):
 
             else:
                 if sample == "EDTA Blood":
-                    logger.info(f"No SMART's FBC results to import.")
+                    logger.info(f"No SMART {case_type}'s  FBC results to import.")
                 else:
-                    logger.info(f"No SMART's BioChemistry results to import.")
+                    logger.info(f"No SMART {case_type}'s BioChemistry results to import.")
 
             # when record successful imported write it to csv(import_data_[date&time])
             # or json file(imported_fbc_data.json). use function for both.
--- redcapconnector/smart_result_transfer.py
+++ redcapconnector/smart_result_transfer.py
@@ -1,6 +1,8 @@
 import os
 import configparser
 import re
+from pprint import pprint
+
 import requests
 import json
 import logging.config
@@ -55,6 +57,9 @@ def get_event_name(pid):
             smart_api_token = os.environ["SMART_UM_API_TOKEN"]
         elif pid_code == "S24-2":
             smart_api_token = os.environ["SMART_AS_API_TOKEN"]
+        else:
+            # RM
+            smart_api_token = os.environ["SMART_RM_API_TOKEN"]
 
         data = {
             'token': smart_api_token,
@@ -62,7 +67,8 @@ def get_event_name(pid):
             'format': 'json',
             'returnFormat': 'json'
         }
-        r = requests.post('https://redcap-testing.bibbox.bnitm.de/api/', data=data)
+        # r = requests.post('https://redcap-testing.bibbox.bnitm.de/api/', data=data)
+        r = requests.post('https://redcap-kccr.bibbox.bnitm.de/api/', data=data)
         # print('HTTP Status: ' + str(r.status_code))
         # print(r.json())
         events = json.loads(json.dumps(r.json()))
@@ -105,6 +111,9 @@ def get_record_id(pid):
         elif pid_code == 'S24-2':
             pid_var = 'h01_3umsm'
             smart_api_token = os.environ["SMART_AS_API_TOKEN"]
+        else:
+            pid_var = 'pid_fup_rm'
+            smart_api_token = os.environ["SMART_RM_API_TOKEN"]
 
         data = {
             'token': smart_api_token,
@@ -152,60 +161,65 @@ def extract_visit_day(sampleid):
 
 @logger.catch
 def extract_event_name(sampleid, visitday, visithr=''):
-    id_arm = get_record_id(sampleid[0:11])
-    if id_arm:
-        id_arm = id_arm[0]["redcap_event_name"][13:]
+    # sampleid -> fnc parameter
+    # id_arm = get_record_id(sampleid[0:11])
 
-        visit_hour = ''
+    # if id_arm:
+    # id_arm = id_arm[0]["redcap_event_name"][13:]
 
-        if visithr != '':
-            if visithr[0:1] == "H":
-                if len(visithr) == 2 and visithr[0:1] == "H":
-                    visit_hour = visithr[-1:]
-                elif len(visithr) == 3 and visithr[0:2] == "H0":
-                    visit_hour = visithr[-1:]
-                else:
-                    visit_hour = visithr[-2:]
-            elif visithr[0:2] == "AL":
-                # ALH00 ALH0
-                if len(visithr[2:].strip()) == 2 and visithr[0:2] == "H":
-                    visit_hour = visithr[-1:]
-                elif len(visithr[2:].strip()) == 3 and visithr[0:2] == "H0":
-                    visit_hour = visithr[-1:]
-                else:
-                    visit_hour = visithr[-2:]
+    id_arm = "arm_1"
+
+    visit_hour = ''
+
+    if visithr != '':
+        if visithr[0:1] == "H":
+            if len(visithr) == 2 and visithr[0:1] == "H":
+                visit_hour = visithr[-1:]
+            elif len(visithr) == 3 and visithr[0:2] == "H0":
+                visit_hour = visithr[-1:]
             else:
-                visit_hour = ''
+                visit_hour = visithr[-2:]
+        elif visithr[0:2] == "AL":
+            # ALH00 ALH0
+            if len(visithr[2:].strip()) == 2 and visithr[0:2] == "H":
+                visit_hour = visithr[-1:]
+            elif len(visithr[2:].strip()) == 3 and visithr[0:2] == "H0":
+                visit_hour = visithr[-1:]
+            else:
+                visit_hour = visithr[-2:]
+        else:
+            visit_hour = ''
 
-        visit_day = visitday
+    visit_day = visitday
 
-        events = get_event_name(sampleid[0:11])
+    events = get_event_name(sampleid[0:11])
 
-        for event in events:
-            evt_s = event.split('_')
-            match evt_s[0]:
-                case "hour":
-                    evt_s_day = '_'.join((evt_s[2], evt_s[3]))
-                    if len(evt_s[1]) == 1 and evt_s[1] == visit_hour and evt_s_day == visit_day and event[-5:] == id_arm:
-                        return event
-                    elif len(evt_s[1]) == 2 and evt_s[1] == visit_hour and evt_s_day == visit_day and event[-5:] == id_arm:
-                        return event
-                case "al":
-                    # al_hour_8_day_0_arm_3 al_hour_24_day_arm_3
-                    evt_s_day = '_'.join((evt_s[3], evt_s[4]))
-                    if len(evt_s[2]) == 1 and evt_s[2] == visit_hour and evt_s_day == visit_day and event[-5:] == id_arm:
-                        return event
-                    elif len(evt_s[2]) == 2 and evt_s[2] == visit_hour and evt_s_day == visit_day and event[-5:] == id_arm:
-                        return event
-                case "day":
-                    evt_s_day = '_'.join((evt_s[0], evt_s[1]))
-                    if evt_s_day == visit_day and event[-5:] == id_arm:
-                        return event
-                    elif evt_s_day == visit_day and event[-5:] == id_arm:
-                        return event
-                case _:
-                    if evt_s[0] == visit_day and event[-5:] == id_arm:
-                        return event
+    for event in events:
+        evt_s = event.split('_')
+        match evt_s[0]:
+            case "hour":
+                evt_s_day = '_'.join((evt_s[2], evt_s[3]))
+                if len(evt_s[1]) == 1 and evt_s[1] == visit_hour and evt_s_day == visit_day and event[-5:] == id_arm:
+                    return event
+                elif len(evt_s[1]) == 2 and evt_s[1] == visit_hour and evt_s_day == visit_day and event[-5:] == id_arm:
+                    return event
+            case "al":
+                # al_hour_8_day_0_arm_3 al_hour_24_day_arm_3
+                evt_s_day = '_'.join((evt_s[3], evt_s[4]))
+                if len(evt_s[2]) == 1 and evt_s[2] == visit_hour and evt_s_day == visit_day and event[-5:] == id_arm:
+                    return event
+                elif len(evt_s[2]) == 2 and evt_s[2] == visit_hour and evt_s_day == visit_day and event[-5:] == id_arm:
+                    return event
+            case "day":
+                # print(id_arm)
+                evt_s_day = '_'.join((evt_s[0], evt_s[1]))
+                if evt_s_day == visit_day and event[-5:] == id_arm:
+                    return event
+                elif evt_s_day == visit_day and event[-5:] == id_arm:
+                    return event
+            case _:
+                if evt_s[0] == visit_day and event[-5:] == id_arm:
+                    return event
 
 
 @logger.catch
@@ -288,16 +302,40 @@ def extract_heparin_visit(sample_id):
     return visit
 
 
+@logger.catch
+def extract_rm_edta_visit(sample_id):
+    visit = ''
+
+    if len(sample_id.strip()) == 11:
+        visit = smart_variables["VISIT_NO_FBC_RM"]["D07"]
+    elif 12 < len(sample_id) <= 15:
+        visit_code = sample_id[-3:]
+        print(visit_code)
+
+        if visit_code[0:1] == '-' or visit_code[0:1] == ' ':
+            visit_code = visit_code[1:].strip()
+            print(visit_code)
+
+        visit_keys = smart_variables["VISIT_NO_FBC_RM"].keys()
+
+        if visit_code in visit_keys:
+            visit = smart_variables["VISIT_NO_FBC_RM"][visit_code]
+
+    return visit
+
+
 def transfer_smart_result(period, sample_type):
     try:
         smart_analysis_data = {}
         um_data = []
         sm_data = []
+        rm_data = []
         next_batch = ''
         case_type = ''
+        data_entry = ''
 
         # items_resp = requests.get(os.environ["BASE_URL"] + "/search", params={"catalog": "senaite_catalog_sample", "getClientTitle": "SMART",
-        items_resp = requests.get("http://172.21.0.1:8091/agogo-test/@@API/senaite/v1/search",
+        items_resp = requests.get(f"{os.environ["BASE_URL"]}/search",
                                   params={"catalog": "senaite_catalog_sample", "getClientTitle": "SMART",
                                           "sort_on": "getDateSampled", "getSampleTypeTitle": sample_type,
                                           "sort_order": "asc", "review_state": "published",
@@ -340,16 +378,27 @@ def transfer_smart_result(period, sample_type):
                     record_ids = get_record_id(client_sample_id[0:11])
                     record_id = ''
                     if record_ids:
-                        record_id = record_ids[0]['id_um']
+                        if len(record_ids) > 1:
+                            record_id = record_ids[1]['id_um']
+
+                            # data entry
+                            data_entry = record_ids[1]["id_um"][-3:]
+                        else:
+                            record_id = record_ids[0]['id_um']
+
+                            # data entry
+                            data_entry = record_ids[0]["id_um"][-3:]
 
                     vhour = res_data_dict_items[item]["getClientReference"]
                     vday = ''
 
-                    # case type [UM|SM]
+                    # case type [UM|SM|RM]
                     if client_sample_id[:5] == "S24-1":
                         case_type = "UM"
                     elif client_sample_id[:5] == "S24-2":
                         case_type = "SM"
+                    elif client_sample_id[:5] == "R01-1":
+                        case_type = "RM"
 
                     if client_sample_id:
                         # S24-2001-AG-D04 S24-2001-AG-D4 S24-2001-AG
@@ -387,8 +436,11 @@ def transfer_smart_result(period, sample_type):
                             # print(vhour)
                             if case_type == "UM":
                                 smart_analysis_data.update({"visit_no_edta": extract_edta_visit(client_sample_id, vday, vhour)})
-                            else:
+                            elif case_type == "SM":
                                 smart_analysis_data.update({"visit_no_sid": extract_heparin_visit(client_sample_id)})
+                            else:
+                                # "RM"
+                                smart_analysis_data.update({"visitno_rm1": extract_heparin_visit(client_sample_id)})
 
                             # children data
                             analysis_counts = res_data_dict_items[item]["children"]
@@ -408,10 +460,19 @@ def transfer_smart_result(period, sample_type):
 
                                     # analysis results/count
                                     fbc_keys = smart_variables["FBC"].keys()
+                                    rm_fbc_keys = smart_variables["FBC_RM"].keys()
                                     fbc_title = re.sub(r'[\[\]]', '', str([analysis_counts[analysis_count]["title"]])).strip("'")
                                     # print(fbc_keys)
-                                    if fbc_title in fbc_keys:
-                                        smart_analysis_data.update({smart_variables["FBC"][analysis_counts[analysis_count]["title"]]: result})
+                                    if case_type == "RM":
+
+                                        if fbc_title in rm_fbc_keys:
+                                            smart_analysis_data.update({smart_variables["FBC"][analysis_counts[analysis_count]["title"]]: result})
+
+                                    else:
+
+                                        if fbc_title in fbc_keys:
+                                            smart_analysis_data.update({smart_variables["FBC"][analysis_counts[analysis_count]["title"]]: result})
+
                                 else:
                                     # sample type -> Heparin Blood
 
@@ -439,18 +500,60 @@ def transfer_smart_result(period, sample_type):
 
                                 # update the record id for the 2nd data entry
                                 # and update the list
-                                record_id_2de = record_ids[1]['id_um']
+                                # record_id_2de = record_ids[1]['id_um']
+
+                                if len(record_ids) > 1:
+                                    record_id_2de = record_ids[1]["id_um"][0:12]
+                                else:
+                                    record_id_2de = record_ids[0]["id_um"][0:12]
+
+                                if data_entry == "--1":
+                                    record_id_2de = f"{record_id_2de}--2"
+                                else:
+                                    record_id_2de = f"{record_id_2de}--1"
+
                                 smart_analysis_data.update({"id_um": record_id_2de})
                                 um_data.append(smart_analysis_data.copy())
-                            else:
+
+                            elif case_type == "SM":
                                 # SM
                                 sm_data.append(smart_analysis_data.copy())
 
                                 # update the record id for the 2nd data entry
                                 # and update the list
-                                record_id_2de = record_ids[1]['id_um']
+                                # record_id_2de = record_ids[1]['id_um']
+
+                                if len(record_ids) > 1:     # check the length to help to choose the right index
+                                    record_id_2de = record_ids[1]["id_um"][0:12]
+                                else:
+                                    record_id_2de = record_ids[0]["id_um"][0:12]
+
+                                if data_entry == "--1":
+                                    record_id_2de = f"{record_id_2de}--2"
+                                else:
+                                    record_id_2de = f"{record_id_2de}--1"
+
                                 smart_analysis_data["id_um"]: record_id_2de
                                 sm_data.append(smart_analysis_data.copy())
+                            else:
+                                # RM
+                                rm_data.append(smart_analysis_data.copy())
+
+                                # update the record id for the 2nd data entry
+                                # and update the list
+
+                                if len(record_ids) > 1:
+                                    record_id_2de = record_ids[1]["id_um"][0:11]
+                                else:
+                                    record_id_2de = record_ids[1]["id_um"][0:11]
+
+                                if data_entry == "--1":
+                                    record_id_2de = f"{record_id_2de}--2"
+                                else:
+                                    record_id_2de = f"{record_id_2de}--1"
+
+                                smart_analysis_data["id_um"]: record_id_2de
+                                rm_data.append(smart_analysis_data.copy())
 
                     # clear the smart_analysis_data dictionary
                     smart_analysis_data.clear()
@@ -463,11 +566,16 @@ def transfer_smart_result(period, sample_type):
         # write to json file
         sm_json_file = os.path.join(os.path.dirname(__file__), "data", "smart_sm_import_data.json")
         um_json_file = os.path.join(os.path.dirname(__file__), "data", "smart_um_import_data.json")
+        rm_json_file = os.path.join(os.path.dirname(__file__), "data", "smart_rm_import_data.json")
+
         if um_data:
             write_json_smart(um_data, um_json_file)
 
         if sm_data:
             write_json_smart(sm_data, sm_json_file)
+
+        if rm_data:
+            write_json_smart(rm_data, rm_json_file)
 
         # importing the data or results into REDCap project database
         # data_import(sample_type)
@@ -480,4 +588,15 @@ def transfer_smart_result(period, sample_type):
 
 
 if __name__ == '__main__':
-    transfer_smart_result("this-year", "EDTA Blood")
+    transfer_smart_result("this-week", "EDTA Blood")
+    # idf = get_record_id("R01-1007-AF")
+    # pprint(idf)
+    # pprint(len(idf))
+    # pprint(idf[0]["id_um"][-3:])
+    # pprint(idf[0]["id_um"][:11])
+    # pprint(idf[0]["id_um"][0:3])
+    # v = extract_rm_edta_visit("R01-1007-AF D7")
+    # print(v)
+    # vd = extract_visit_day("S24-1007-AG-D7")
+    # evn = extract_event_name("S24-1007-AG-D7", vd, "")
+    # print(evn)
